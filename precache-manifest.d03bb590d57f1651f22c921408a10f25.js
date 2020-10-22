0a851ae24039fccfa2230f97514d666026369c17
Tsiresy RABEKOTO <tsiresyrabekoto@gmail.com>
2020-10-22 07:35:15 +0200

    Updates

---
+++ precache-manifest.d03bb590d57f1651f22c921408a10f25.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ced088b31dd3d83701a7631c5dfa2a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "63576913b696d8291994",
+    "url": "/Portfolio/static/css/main.6b7b6148.chunk.css"
+  },
+  {
+    "revision": "51e14b40d3d96f08b671",
+    "url": "/Portfolio/static/js/2.c0deb112.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/Portfolio/static/js/2.c0deb112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63576913b696d8291994",
+    "url": "/Portfolio/static/js/main.e1586682.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/Portfolio/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/Portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/Portfolio/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/Portfolio/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/Portfolio/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "5ff333d9bb3a1446432fccbcf7244532",
+    "url": "/Portfolio/static/media/magazine-septembre-2020.5ff333d9.webp"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/Portfolio/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/Portfolio/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/Portfolio/static/media/react.f370c1e0.png"
+  },
+  {
+    "revision": "1ded69cbe0f6b1061b4c7cfbd726c986",
+    "url": "/Portfolio/static/media/sorbonne.1ded69cb.png"
+  }
+]);
